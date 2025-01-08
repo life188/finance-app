00ea23f86c93d9ed64f17730321a32ab15a0ec62
= <aalna096@uottawa.ca>
2025-01-08 15:37:45 -0500

    Created the sign in and Sign out pages using Clerk as well as made sure the user in loged in before he sees the dashboard

---
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,31 @@
+import { SignIn, ClerkLoaded, ClerkLoading } from "@clerk/nextjs";
+import { Loader2 } from "lucide-react";
+import Image from "next/image";
+
+export default function Page() {
+  return (
+    <div className="min-h-screen grid grid-cols-1 lg:grid-cols-2 bg-green-100">
+      <div className="h-full lg:flex flex-col items-center justify-center px-4">
+        <div className="text-center space-y-4 pt-16">
+          <h1 className="font-bold text-3xl text-[#2E2A47]">
+            Welcome to SpendSmart
+          </h1>
+          <p className="text-base text-[#7E8CA0]">
+            Login or create account to access dashboard.
+          </p>
+        </div>
+        <div className="flex items-center justify-center mt-8">
+          <ClerkLoaded>
+            <SignIn path="/sign-in" />
+          </ClerkLoaded>
+          <ClerkLoading>
+            <Loader2 className="animate-spin text-muted-foreground" />
+          </ClerkLoading>
+        </div>
+      </div>
+      <div className="h-full  lg:flex hidden items-center justify-center bg-green-400 ">
+        <img src="logo.png" alt="logo" width="400" height="100" />
+      </div>
+    </div>
+  );
+}
