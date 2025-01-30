1445e7ba475ace2eaf7379759c9f61d36a49285b
= <aalna096@uottawa.ca>
2025-01-30 12:05:42 -0500

    finished transaction API as well as transaction Hooks

--- db/schema.ts
+++ db/schema.ts
@@ -1,5 +1,7 @@
-import { pgTable, text } from 'drizzle-orm/pg-core';
+import { integer, pgTable, text, timestamp } from 'drizzle-orm/pg-core';
 import { createInsertSchema } from 'drizzle-zod';
+import { relations } from 'drizzle-orm';
+import { z } from 'zod';
 
 export const accounts = pgTable('account', {
   id: text('id').primaryKey(),
@@ -8,6 +10,9 @@ export const accounts = pgTable('account', {
   userId: text('user_id').notNull(),
 });
 
+export const accountRelations = relations(accounts, ({ many }) => ({
+  transactions: many(transactions),
+}));
 export const insertAccountSchema = createInsertSchema(accounts);
 
 export const categories = pgTable('categories', {
@@ -16,4 +21,36 @@ export const categories = pgTable('categories', {
   name: text('name').notNull(),
   userId: text('user_id').notNull(),
 });
+export const categoriesRelations = relations(categories, ({ many }) => ({
+  transactions: many(transactions),
+}));
 export const insertCategoriesSchema = createInsertSchema(categories);
+
+export const transactions = pgTable('transactions', {
+  id: text('id').primaryKey(),
+  amount: integer('amount').notNull(),
+  payee: text('payee').notNull(),
+  notes: text('notes'),
+  date: timestamp('date', { mode: 'date' }).notNull(),
+  accountId: text('account_id')
+    .references(() => accounts.id, { onDelete: 'cascade' })
+    .notNull(),
+  categoryId: text('category_id').references(() => categories.id, {
+    onDelete: 'set null',
+  }),
+});
+
+export const transactionsRelations = relations(transactions, ({ one }) => ({
+  account: one(accounts, {
+    fields: [transactions.accountId],
+    references: [accounts.id],
+  }),
+  categories: one(categories, {
+    fields: [transactions.categoryId],
+    references: [categories.id],
+  }),
+}));
+
+export const insertTransactionSchema = createInsertSchema(transactions, {
+  date: z.coerce.date(),
+});
